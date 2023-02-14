3409ca29e05731717853879402dbe9ceff9d4740
raphaelDupuy <raphael.dupuy@ens.uvsq.fr>
2023-02-14 13:53:49 +0100

    ajout fonction analyse

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from sommet import Sommet
 from arbreB import ArbreB
 
 
+data = {}
 
+def analyse(file):
+    fichier = open(file, 'r+')
+    for ligne in fichier:
+        print(ligne)
 
+joli_arbre = ArbreB
 
-
+analyse("texte.txt")
 
 
 
