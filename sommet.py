9523fdfcefeb15235f76c8e2e6b1ed0e11ebcc8c
raphaelDupuy <raphael.dupuy@ens.uvsq.fr>
2023-02-14 11:03:10 +0100

    upgrades

--- sommet.py
+++ sommet.py
@@ -1,8 +1,16 @@
 class Sommet:
 
-    def __init__(self, tag):
+    def __init__(self, nb, chemin, tag=None):
         self.tag = tag
+        self.nb = nb
+        self.chemin = chemin
 
     def retag(self, new):
         self.tag = new
+
+    def get_chemin(self):
+        return self.chemin
+
+    def get_tag(self):
+        return self.tag
 
