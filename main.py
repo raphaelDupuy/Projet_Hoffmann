c839df87153f2d2840de821075dc864d9232de65
raphaelDupuy <raphael.dupuy@ens.uvsq.fr>
2023-02-15 09:45:17 +0100

    Created function extract()

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from sommet import Sommet
 from arbreB import ArbreB
 
+total = 0
 
-data = {}
+def extract(file):
+    global total
+    data = {}
+    with open(file, "r") as fichier:
+        for line in fichier:
+            for letter in line:
+                low_letter = letter.lower()
+                if low_letter not in data.keys():
+                    data[letter] = 1
+                else:
+                    data[letter] += 1
+                total += 1
+    return data
+
 
 def analyse(file):
-    fichier = open(file, 'r+')
-    for ligne in fichier:
-        print(ligne)
+    data = extract(file)
+    print(data)
+    for key, value in data.items():
+        pass
+
 
 joli_arbre = ArbreB
 
