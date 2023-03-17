dc5a9bba0c9751f2e671a9876101e0478ca0f25c
uvsq22101259 <elie.kanga@ens.uvsq.fr>
2023-03-17 11:00:38 +0100

    compte rendu commencé

--- int_graph.py
+++ int_graph.py
@@ -1,33 +1,33 @@
-import tkinter as tk
-from sommet import Sommet
-from arbreB import ArbreB
+# import tkinter as tk
+# from sommet import Sommet
+# from arbreB import ArbreB
 
 
 
-def creation_arbre(arbre, x = 1000,y = 100, range  = 500 ):
-        t = 10
-        if x == 1000 :
-            screen.create_oval((x-t,30-t),(x+t,30+t))
+# def creation_arbre(arbre, x = 700,y = 100, range  = 300 ):
+#         t = 10
+#         if x == 500 :
+#             screen.create_oval((x-t,30-t),(x+t,30+t))
 
-        fg = arbre.get_fg()
-        fg_tag = fg.get_tag()
-        fd = arbre.get_fd()
-        fd_tag = fd.get_tag()
-        xfd = x + range
-        xfg = x - range
+#         fg = arbre.get_fg()
+#         fg_tag = fg.get_tag()
+#         fd = arbre.get_fd()
+#         fd_tag = fd.get_tag()
+#         xfd = x + range
+#         xfg = x - range
         
-        if fd_tag == None:
-            screen.create_oval((xfd-t,y-t),(xfd+t,y+t))
-            creation_arbre(fd,xfd, y+60, range //2)
-            screen.create_text(xfd,y, text= fd.get_occur())
-        else:
-            screen.create_text(xfd,y, text= fd_tag)
-        if fg_tag == None:
-            screen.create_oval((xfg-t ,y-t),(xfg+t,y+t))
-            creation_arbre(fg,xfg, y+60, range //2)
-            screen.create_text(xfg,y, text= fg.get_occur())
-        else:
-            screen.create_text(xfg,y, text= fg_tag)
+#         if fd_tag == None:
+#             screen.create_oval((xfd-t,y-t),(xfd+t,y+t))
+#             creation_arbre(fd,xfd, y+60, range //2)
+#             screen.create_text(xfd,y, text= fd.get_occur())
+#         else:
+#             screen.create_text(xfd,y, text= fd_tag)
+#         if fg_tag == None:
+#             screen.create_oval((xfg-t ,y-t),(xfg+t,y+t))
+#             creation_arbre(fg,xfg, y+60, range //2)
+#             screen.create_text(xfg,y, text= fg.get_occur())
+#         else:
+#             screen.create_text(xfg,y, text= fg_tag)
 
     
         
@@ -35,136 +35,161 @@ def creation_arbre(arbre, x = 1000,y = 100, range  = 500 ):
 
 
 
-total = 0
+# total = 0
 
 
-# Prend en entrée un fichier texte et retourne une liste de sommets correspondant 
-# aux charactères dans le fichier
-def extract(file):
-    global total,arbre
-    data = {}
-    with open(file, "r") as fichier:
-        for line in fichier:
-            for letter in line:
-                low_letter = letter.lower()
-                if low_letter not in data.keys():
-                    data[low_letter] = 1
-                else:
-                    data[low_letter] += 1
-                total += 1
+# # Prend en entrée un fichier texte et retourne une liste de sommets correspondant 
+# # aux charactères dans le fichier
+# def extract(file):
+#     global total,arbre
+#     data = {}
+#     with open(file, "r") as fichier:
+#         for line in fichier:
+#             for letter in line:
+#                 low_letter = letter.lower()
+#                 if low_letter not in data.keys():
+#                     data[low_letter] = 1
+#                 else:
+#                     data[low_letter] += 1
+#                 total += 1
 
-    classes = []
-    for key, value in data.items():
-        classes.append(Sommet(value, tag=key))
-    return classes
-
-
-# Prend en entrée une liste d'objets sommets et le nombre total de charactères dans le fichier
-# à analyser et retourne un dictionnaire avec le char en clé et son pourcentage en value
-def percentage(data):
-    global total
-    for sommet in data:
-        sommet.occur = round((sommet.get_occur()/total)*100,2)
-    return data
-
-def tree(data):
-    pass
-
-# Fonction principale du programme
-def analyse(file):
-    data = extract(file)
-    return percentage(data)
+#     classes = []
+#     for key, value in data.items():
+#         classes.append(Sommet(value, tag=key))
+#     return classes
 
 
-def getKey(element):
-    """indique a la fonction sort() sur quel paramètres elle doit trier la liste"""
-    return element.get_occur()
+# # Prend en entrée une liste d'objets sommets et le nombre total de charactères dans le fichier
+# # à analyser et retourne un dictionnaire avec le char en clé et son pourcentage en value
+# def percentage(data):
+#     global total
+#     for sommet in data:
+#         sommet.occur = round((sommet.get_occur()/total)*100,2)
+#     return data
+
+# def tree(data):
+#     pass
+
+# # Fonction principale du programme
+# def analyse(file):
+#     data = extract(file)
+#     return percentage(data)
 
 
-arbre = None
-def build_tree(list_sommet):
-    """version 1"""
-    # """ prends en entré une liste de sommets et crèe un arbre avec,
-    #   les  2 sommets dont l'occurence est la plus faible"""
-    # global joli_arbre
-    # list_sommet = sorted(list_sommet, key=getKey)
-    # fg = (list_sommet[0][1][1])
-    # fd = (list_sommet[1][1][1])
-    # joli_arbre = ArbreB(fd,fg)
-    # if isinstance(fg, ArbreB):
-    #     fg = fg.get_sommet()
-    # if isinstance(fd, ArbreB):
-    #     fd = fd.get_sommet()
-    # joli_arbre.sommet = Sommet((fg.get_occur() + fd.get_occur() ))
-    # list_sommet.append((joli_arbre.get_tag(),(joli_arbre.get_sommet().get_occur(),joli_arbre)))
-    # del(list_sommet[:2])
-
-    # if len(list_sommet) != 1:
-    #     build_tree(list_sommet)
-
-    """version 2 recursif test"""
-    """prend en entrée une liste non triée de sommets"""
-    if len(list_sommet) > 1:
-        small = list_sommet[1]
-        small_occur = small.get_occur()
-
-        smaller = list_sommet[0]
-        smaller_occur = smaller.get_occur()
-
-        for sommet in list_sommet[2:]:
-            occur = sommet.get_occur()
-
-            if occur < smaller_occur:
-                smaller = sommet
-
-            elif occur < small_occur:
-                small = sommet
-
-        sous_arbre = ArbreB(list_sommet.pop(list_sommet.index(small)), list_sommet.pop(list_sommet.index(smaller)), Sommet(round(small.get_occur() + smaller.get_occur(),2)))
-        list_sommet.append(sous_arbre)
-        return build_tree(list_sommet)
-
-    else:
-        return list_sommet[0]        
+# def getKey(element):
+#     """indique a la fonction sort() sur quel paramètres elle doit trier la liste"""
+#     return element.get_occur()
 
 
+# arbre = None
+# def build_tree(list_sommet):
+#     """version 1"""
+#     # """ prends en entré une liste de sommets et crèe un arbre avec,
+#     #   les  2 sommets dont l'occurence est la plus faible"""
+#     # global joli_arbre
+#     # list_sommet = sorted(list_sommet, key=getKey)
+#     # fg = (list_sommet[0][1][1])
+#     # fd = (list_sommet[1][1][1])
+#     # joli_arbre = ArbreB(fd,fg)
+#     # if isinstance(fg, ArbreB):
+#     #     fg = fg.get_sommet()
+#     # if isinstance(fd, ArbreB):
+#     #     fd = fd.get_sommet()
+#     # joli_arbre.sommet = Sommet((fg.get_occur() + fd.get_occur() ))
+#     # list_sommet.append((joli_arbre.get_tag(),(joli_arbre.get_sommet().get_occur(),joli_arbre)))
+#     # del(list_sommet[:2])
 
-def unravell(arbre,n,chemin = ""):
-    fg = arbre.get_fg()
-    fg_tag = fg.get_tag()
-    fd = arbre.get_fd()
-    fd_tag = fd.get_tag()
+#     # if len(list_sommet) != 1:
+#     #     build_tree(list_sommet)
 
-    if fg_tag == None:
-        print(n*" ",(fg.get_occur(),fg_tag))
-        unravell(fg,n+5,chemin+ "0")
-    else:
-        print(n*" " ,(fg_tag,fg.get_occur()),chemin + "0")
+#     """version 2 recursif test"""
+#     """prend en entrée une liste non triée de sommets"""
+#     if len(list_sommet) > 1:
+#         small = list_sommet[1]
+#         small_occur = small.get_occur()
+
+#         smaller = list_sommet[0]
+#         smaller_occur = smaller.get_occur()
+
+#         for sommet in list_sommet[2:]:
+#             occur = sommet.get_occur()
+
+#             if occur < smaller_occur:
+#                 smaller = sommet
+
+#             elif occur < small_occur:
+#                 small = sommet
+
+#         sous_arbre = ArbreB(list_sommet.pop(list_sommet.index(small)), list_sommet.pop(list_sommet.index(smaller)), Sommet(round(small.get_occur() + smaller.get_occur(),2)))
+#         list_sommet.append(sous_arbre)
+#         return build_tree(list_sommet)
+
+#     else:
+#         return list_sommet[0]        
+
+
+
+# def unravell(arbre,n,chemin = ""):
+#     fg = arbre.get_fg()
+#     fg_tag = fg.get_tag()
+#     fd = arbre.get_fd()
+#     fd_tag = fd.get_tag()
+
+#     if fg_tag == None:
+#         print(n*" ",(fg.get_occur(),fg_tag))
+#         unravell(fg,n+5,chemin+ "0")
+#     else:
+#         print(n*" " ,(fg_tag,fg.get_occur()),chemin + "0")
     
-    if fd_tag == None:
-        print(n*" ",(fd.get_occur(),fd_tag))
-        unravell(fd,n+5,chemin +"1")
-    else:
-        print(n*" " , (fd_tag,fd.get_occur()), chemin +"1")
+#     if fd_tag == None:
+#         print(n*" ",(fd.get_occur(),fd_tag))
+#         unravell(fd,n+5,chemin +"1")
+#     else:
+#         print(n*" " , (fd_tag,fd.get_occur()), chemin +"1")
 
 
 
-# six = ArbreB(f, c, Sommet(f.get_occur() + c.get_occur()))
+# # six = ArbreB(f, c, Sommet(f.get_occur() + c.get_occur()))
 
-joli_arbre = analyse("texte.txt")
-joli_arbre = build_tree(joli_arbre)
-unravell(joli_arbre,3)
-
-
+# joli_arbre = analyse("texte.txt")
+# joli_arbre = build_tree(joli_arbre)
+# unravell(joli_arbre,3)
 
 
 
-racine = tk.Tk()
 
-menu = tk.Button(racine, text=" creation arbre")
-menu.grid(column = 0, row = 0 )
-screen = tk.Canvas(racine , width= 2000, height= 1000 , bg= "white")
-screen.grid(column = 1, row = 0, rowspan = 20)
-creation_arbre(joli_arbre)
 
-racine.mainloop()
+# racine = tk.Tk()
+
+# menu = tk.Button(racine, text=" creation arbre")
+# menu.grid(column = 0,  row = 0 )
+# screen = tk.Canvas(racine , width= 2000, height= 1000 , bg= "white", scrollregion=(1000,1000,500,500))
+# hbar=tk.Scrollbar(racine,orient= "horizontal")
+# hbar.pack(side= "bottom",fill="x")
+# hbar.config(command=screen.xview)
+# vbar=tk.Scrollbar(racine,orient="vertical")
+# vbar.pack(side="right",fill="y")
+# screen.grid(column = 1, row = 0, rowspan = 20)
+# creation_arbre(joli_arbre)
+
+# racine.mainloop()
+
+from tkinter import *
+root=Tk()
+frame=Frame(root,width=3000,height=3000)
+menu = Button(root, text=" creation arbre")
+menu.pack(side= 'top', )
+frame.pack(expand=True, fill=BOTH) #.grid(row=0,column=0)
+canvas=Canvas(frame,bg='#FFFFFF',width=1000,height=1000,scrollregion=(0,0,500,500))
+canvas.create_line((2,2), (998,998))
+hbar=Scrollbar(frame,orient=HORIZONTAL)
+hbar.pack(side=BOTTOM,fill=X)
+hbar.config(command=canvas.xview)
+vbar=Scrollbar(frame,orient=VERTICAL)
+vbar.pack(side=RIGHT,fill=Y)
+vbar.config(command=canvas.yview)
+canvas.config(width=300,height=300)
+canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+canvas.pack(side=LEFT,expand=True,fill=BOTH)
+
+root.mainloop()
