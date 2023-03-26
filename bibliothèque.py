d5c880615916a51ca3ab942b70d5a33417ac322d
raphaelDupuy <raphael.dupuy@ens.uvsq.fr>
2023-03-26 14:32:14 +0200

    création bibliothèque

---
+++ bibliothèque.py
@@ -0,0 +1,142 @@
+from sommet import Sommet
+from arbreB import ArbreB
+total = 0
+
+
+def suppr(arbreB, noeud):
+
+    if arbreB.get_tag() == noeud:
+        if type(arbreB) == ArbreB:
+            return arbreB.suppr()
+        else:
+            return None
+
+    else:
+        fg = arbreB.get_fg()
+        fd = arbreB.get_fd()
+
+        if type(fg) == Sommet:
+            print(f"fg est sommet {fg.get_tag()}")
+            if fg.get_tag() == noeud:
+                arbreB.set_content(fg=None, fd=fd)
+                print("done")
+
+        if type(fd) == Sommet:
+            print(f"fd est sommet {fd.get_tag()}")
+            if fd.get_tag() == noeud:
+                arbreB.set_content(fg=fg, fd=None)
+                print("done")
+        
+        if type(fg) == ArbreB:
+            print("continue sur fg")
+            suppr(fg, noeud)
+
+        if type(fd) == ArbreB:
+            print("continue sur fd")
+            suppr(fd, noeud)
+
+
+# Prend en entrée un fichier texte et retourne une liste de sommets correspondant 
+# aux charactères dans le fichier
+def extract(file):
+    global total,arbre, dictionnaire
+    data = {}
+    with open(file, "r") as fichier:
+        for line in fichier:
+            for letter in line:
+                low_letter = letter.lower()
+                if low_letter not in data.keys():
+                    data[low_letter] = 1
+                else:
+                    data[low_letter] += 1
+                dictionnaire[low_letter] = ""
+                total += 1
+
+    classes = []
+    for key, value in data.items():
+        classes.append(Sommet(value, tag=key))
+    return classes
+
+
+# Prend en entrée une liste d'objets sommets et le nombre total de charactères dans le fichier
+# à analyser et retourne un dictionnaire avec le char en clé et son pourcentage en value
+def percentage(data):
+    global total
+    for sommet in data:
+        sommet.occur = round((sommet.get_occur()/total)*100,2)
+    return data
+
+
+# Fonction principale du programme
+def analyse(file):
+    data = extract(file)
+    return percentage(data)
+
+
+def getKey(element):
+    """indique a la fonction sort() sur quel paramètres elle doit trier la liste"""
+    return element.get_occur()
+
+
+arbre = None
+def build_tree(list_sommet):
+    """version 1"""
+    # """ prends en entré une liste de sommets et crèe un arbre avec,
+    #   les  2 sommets dont l'occurence est la plus faible"""
+    # global joli_arbre
+    # list_sommet = sorted(list_sommet, key=getKey)
+    # fg = (list_sommet[0][1][1])
+    # fd = (list_sommet[1][1][1])
+    # joli_arbre = ArbreB(fd,fg)
+    # if isinstance(fg, ArbreB):
+    #     fg = fg.get_sommet()
+    # if isinstance(fd, ArbreB):
+    #     fd = fd.get_sommet()
+    # joli_arbre.sommet = Sommet((fg.get_occur() + fd.get_occur() ))
+    # list_sommet.append((joli_arbre.get_tag(),(joli_arbre.get_sommet().get_occur(),joli_arbre)))
+    # del(list_sommet[:2])
+
+    # if len(list_sommet) != 1:
+    #     build_tree(list_sommet)
+
+    """version 2 recursif test"""
+    """prend en entrée une liste non triée de sommets"""
+    if len(list_sommet) > 1:
+        sorted(list_sommet, key=getKey)
+        small = list_sommet[1]
+        smaller = list_sommet[0]
+        sous_arbre = ArbreB(list_sommet.pop(list_sommet.index(small)), list_sommet.pop(list_sommet.index(smaller)), Sommet(small.get_occur() + smaller.get_occur()))
+        list_sommet.append(sous_arbre)
+        return build_tree(list_sommet)
+
+    else:
+        return list_sommet[0]
+    
+# Permet de dérouller le chemin d'un arbre
+def unravell(arbre,n,chemin = ""):
+    fg = arbre.get_fg()
+    fg_tag = fg.get_tag()
+    fd = arbre.get_fd()
+    fd_tag = fd.get_tag()
+
+    if fg_tag == None:
+        print(n*" ",(fg.get_sommet().get_occur(),fg_tag))
+        unravell(fg,n+5,chemin+ "0")
+    else:
+        print(n*" " ,(fg_tag,fg.get_occur()),chemin + "0")
+    
+    if fd_tag == None:
+        print(n*" ",(fd.get_sommet().get_occur(),fd_tag))
+        unravell(fd,n+5,chemin +"1")
+    else:
+        print(n*" " , (fd_tag,fd.get_occur()), chemin +"1")
+
+
+def creation_dictionnaire(arbre: ArbreB):
+    for key in dictionnaire.keys():
+        dictionnaire[key] = arbre.find(key)
+    print(dictionnaire)
+
+def codage(arbre,charactere):
+    pass
+
